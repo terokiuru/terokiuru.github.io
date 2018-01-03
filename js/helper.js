7be6bd5bc163626337d6b21dc00abc0696f9467a
tkterot <tero.kiuru@vtt.fi>
2018-01-03 10:47:36 +0200

    commit with all files

---
+++ js/helper.js
@@ -0,0 +1,13 @@
+function UserAction() {
+	location.href = "C:\\DATA\\Superennustaja\\nettisivudraftit\\nettisivut_bootsrtrap_1\\questions.html";
+}
+
+// function upDate() {
+// 	console.log("moii!");
+// }
+
+
+var intro_items;
+intro_items = $("#intro_text");
+intro_items_2 = intro_items.find("p")
+intro_items_2.css("font-size", "30px");
---
+++ js/helper2.js
@@ -0,0 +1,48 @@
+var number_of_questions = 0;
+var $top = $('.container-fluid');
+var string_1 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6" > <p>%data%</p><input type="text" id="%id%" value=""></div><div class="col-md-3"></div></div>';
+var string_2 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <hr> </div><div class="col-md-3"></div></div>';
+var submit_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <form onclick="collect_data()"> <input type="button" value="Lähetä vastaukset"></form> </div><div class="col-md-3"></div></div>';
+var json_answers = '{"email": "info@superennustaja.fi","answers": %json_array%}'
+var thank_you_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6 text-center"> <br> <h1>Kiitos vastauksista!</h1> </div><div class="col-md-3"></div></div>';
+
+function collect_data() {
+	var jsonArr = [];
+	for (var i = 0; i<number_of_questions; i++) {
+		jsonArr.push({
+			id: i+1,
+			answer: document.getElementById(i+1).value
+		});
+	}
+	json_text = JSON.stringify(jsonArr);
+	json_answers = json_answers.replace("%json_array%", json_text);
+	console.log(json_answers);
+	$.post( "https://gb5nzln27b.execute-api.eu-west-1.amazonaws.com/test/survey/1/answers", json_answers);
+
+	$( ".container-fluid" ).empty();
+	$( ".container-fluid" ).append(thank_you_string);
+
+	setTimeout(function () {
+		window.location.href = "C:\\DATA\\Superennustaja\\nettisivudraftit\\nettisivut_bootsrtrap_1\\index.html";
+	}, 4000); //will call the function after 4 secs.
+}
+
+function up_date() {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", "https://gb5nzln27b.execute-api.eu-west-1.amazonaws.com/test/survey/1/questions", false);
+	xhr.send();
+	var response = JSON.parse(xhr.responseText);
+	response = JSON.parse(response);
+	var questions = response.questions;
+	number_of_questions = questions.length;
+	var append_string;
+	for (var i = 0; i<number_of_questions; i++) {
+		append_string = string_1  + string_2
+		id =questions[i].id;
+		text = questions[i].text_fi;
+		append_string = append_string.replace("%data%", text);
+		append_string = append_string.replace("%id%", id)
+		$top.append(append_string);
+	}
+	$top.append(submit_string);
+}
