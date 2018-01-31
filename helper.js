4aa3d7263ce3e4d89cdfd04f80a9b7d55d3c0043
terokiuru <tero.kiuru@gmail.com>
2018-01-31 16:09:09 +0200

    Finally working sliders. problem was resetting first sliders in helper.js

--- helper.js
+++ helper.js
@@ -1,10 +1,12 @@
 var number_of_questions = 0;
 var question_strings = [];
 var slider_id_json = [];
+var slider_ids = [];
 var $top = $('.container-fluid');
-// var string_1 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6" > <p>%data%</p><input type="text" id="%id%"> % </div><div class="col-md-3"></div></div>';
-var string_1 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6" > <p>%data%</p><input type="range" min="0" max="100" value="50" class="slider" id="%id%"><p>Todennäköisyys: <span id="demo%id2%"></span> %</p> </div><div class="col-md-3"></div></div>';
-var string_2 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <hr> </div><div class="col-md-3"></div></div>';
+// var content_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6" > <p>%data%</p><input type="text" id="%id%"> % </div><div class="col-md-3"></div></div>';
+var script_string = '<script> var slider%id3% = document.getElementById("%id4%"); var output%id5% = document.getElementById("demo%id6%"); output%id7%.innerHTML = slider%id8%.value; slider%id9%.oninput = function() {output%id10%.innerHTML = this.value;}</script>'
+var content_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6" > <p>%data%</p> <div class="slidecontainer"> <input type="range" min="0" max="100" value="50" class="slider" id="%id%"><p>Todennäköisyys: <span id="demo%id2%"></span> %</p></div> %script% </div><div class="col-md-3"></div></div> ';
+var hr_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <hr> </div><div class="col-md-3"></div></div>';
 var string_3 = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <br> </div><div class="col-md-3"></div></div>';
 var submit_string = '<div class="row"> <div class="col-md-3"> </div> <div class="col-md-6"> <form onclick="collect_data()"> <input type="button" value="Lähetä vastaukset"></form> </div><div class="col-md-3"></div></div>';
 var json_answers = '{"email": "%email_string%", "user_name": "test_name", "answer_time": "%time_string%", "extra_info_1": "test_info_1", "questions_and_answers": %json_array%}'
@@ -45,23 +47,33 @@ function up_date() {
 	var questions = response.questions;
 	number_of_questions = questions.length;
 	var append_string;
-	for (var i = 0; i<number_of_questions; i++) {
-		append_string = string_1  + string_2
-		id =questions[i].id;
+	for (var i = 0; i < number_of_questions; i++) {
+		slider_ids[i] = i;
+		append_string = content_string + hr_string;
+		id = questions[i].id;
 		text = questions[i].text_fi;
 		question_strings[i] = text;
+		append_string = append_string.replace("%script%", script_string)
 		append_string = append_string.replace("%data%", text);
-		append_string = append_string.replace("%id%", id)
-		append_string = append_string.replace("%id2%", id)
+		append_string = append_string.replace("%id%", id);
+		append_string = append_string.replace("%id2%", id);
+		append_string = append_string.replace("%id3%", id);
+		append_string = append_string.replace("%id4%", id);
+		append_string = append_string.replace("%id5%", id);
+		append_string = append_string.replace("%id6%", id);	
+		append_string = append_string.replace("%id7%", id);
+		append_string = append_string.replace("%id8%", id);
+		append_string = append_string.replace("%id9%", id);
+		append_string = append_string.replace("%id10%", id);						
 		$top.append(append_string);
-		slider = document.getElementById(i+1);
-		output = document.getElementById("demo"+ (i+1));
-		output.innerHTML = slider.value;
-		slider.oninput = function() {
-			output.innerHTML = this.value;
-		}
+		// slider = document.getElementById(i+1);
+		// output = document.getElementById("demo"+ (i+1));
+		// output.innerHTML = slider.value;
+		// slider.oninput = function() {
+		// 	output.innerHTML = this.value;
+		// }
 	}
-	// console.log(slider_id_json);
+	console.log(slider_ids);
 	$top.append(submit_string + string_3);
 
 }
@@ -74,8 +86,8 @@ function S4() {
 	return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
 }
 
-var slider;
-var output;
+// var slider;
+// var output;
 
 // document.addEventListener("mousedown", function(){
 // 	for (var i = 0; i<number_of_questions+1; i++) {
@@ -101,22 +113,23 @@ var output;
 // 	}
 // });
 
-document.addEventListener("mousedown", function(){
-	for (var i = 0; i<number_of_questions+1; i++) {
-		slider = document.getElementById(i+1);
-		output = document.getElementById("demo"+ (i+1));
-		if (slider != null) {
-			output.innerHTML = slider.value;
-			slider.oninput = function() {
-				if (output != null) {
-					output.innerHTML = this.value;
-				}
-			}
-		}
-	}
-});
+// document.addEventListener("click", function(){
+// 	for (var i = 0; i<number_of_questions+1; i++) {
+// 		slider = document.getElementById(i+1);
+// 		output = document.getElementById("demo"+ (i+1));
+// 		if (slider != null) {
+// 			output.innerHTML = slider.value;
+// 			slider.oninput = function() {
+// 				if (output != null) {
+// 					output.innerHTML = this.value;
+// 				}
+// 			}
+// 		}
+// 	}
+// });
 
 // var inp = document.getElementById('rangevalue');
 // inp.addEventListener("mousemove", function () {
 //     document.getElementById('text').innerHTML = this.value;
 // });
+
